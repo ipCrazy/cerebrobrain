cf6d21bb943f25a6e35a66d43a46ced5b351e14c
ip380 <ip5015316@gmail.com>
2025-06-09 12:30:42 +0200

    Login/Register front end, shadcn install

---
+++ app/register/page.tsx
@@ -0,0 +1,18 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import { Brain } from "lucide-react";
+
+import { SignUpForm } from "@/components/signup-form";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <Brain size={24} color="currentColor" strokeWidth={2} />
+          Cerebro
+        </a>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
---
+++ components/signup-form.tsx
@@ -0,0 +1,75 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function SignUpForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Create an account</CardTitle>
+          <br />
+          <CardDescription>
+            Make sure your password is strong and unique.
+          </CardDescription>
+          <br />
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="name">Name</Label>
+                  <Input
+                    id="name"
+                    type="text"
+                    placeholder="Your full name"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" required />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="confirmPassword">Confirm Password</Label>
+                  <Input id="confirmPassword" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Sign Up
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Already have an account?{" "}
+                <Link href="/login">
+                  <span className="underline underline-offset-4">Login</span>
+                </Link>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
